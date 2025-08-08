88054f8bb8906141e2410b828f7d0c1fa5d19229
Adithiya-A <a.adithiya1508@gmail.com>
2025-08-08 22:36:08 +0530

    feat: add login page

---
+++ frontend/app/(auth)/login.tsx
@@ -0,0 +1,102 @@
+import { Pressable, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import Icon from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome5'
+
+
+//Themed Components
+import Spacer from "../../components/spacer"
+import ThemedTextInput from "../../components/ThemedTextInput"
+
+
+const Login = () => {
+
+  const [registerNumber, setRegisterNumber] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit =() => {
+    console.log("User : ", registerNumber, "Pass : ", password, "Logged In")
+  }
+
+
+  return (
+    <View style={styles.container}>
+
+    <Icon name="calendar-outline" size={100} color="#9e0202" />
+
+    <Spacer/>
+
+    <Text style={styles.title}>Prathyusha Events</Text>
+
+    <Spacer height={10}/>
+
+    <Text>Sign in to continue</Text>
+
+    <Spacer height={40}/>
+    
+    <View style={styles.inputContainer}>
+      <Icon name='person' size={20} style={[{marginHorizontal: 10}, {color: '#65758c'}]}/>
+      <ThemedTextInput placeholder="Registration Number" placeholderTextColor={'#65758c'} style={{width:"80%"}}
+      onChangeText={setRegisterNumber} value={registerNumber}/>
+    </View>
+
+    <Spacer height={20}/>
+
+    <View style={styles.inputContainer}>
+      <FontAwesome name='lock' size={20} style={[{marginHorizontal: 10}, {color: '#65758c'}]}/>
+      <ThemedTextInput placeholder="Password" placeholderTextColor={'#65758c'} style={{width:"80%"}}
+      onChangeText={setPassword} value={password} secureTextEntry/>
+    </View>
+
+    <Spacer height={40}/>
+
+    <Pressable onPress={handleSubmit} style={({pressed}) => [styles.btn, pressed && styles.pressed] }>
+      <Text style={styles.btnText}>Login</Text>
+    </Pressable>
+
+    <Spacer height={30}/>
+
+    <Text style={{color:'#9e0202'}}> Forgot Password?</Text>
+
+    </View>
+  )
+}
+
+export default Login
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    inputContainer:{
+      alignItems: 'center',
+      flexDirection: 'row',
+      backgroundColor:"#e6e6e6",
+      color: "black",
+      padding: 10,
+      borderRadius: 10,
+      width: '80%'
+    },
+
+    title: {
+        fontWeight: "bold",
+        fontSize: 28,
+    },
+    btnText:{
+      textAlign:"center",
+      fontWeight:"bold",
+      color: '#ffffff'
+    },
+    btn:{
+      backgroundColor: '#9e0202',
+      width: '80%',
+      borderRadius: 10,
+      padding: 15,
+    },
+    pressed:{
+      opacity: 0.7
+    }
+})
--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,12 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import TestRefComponent from './test-ref'
+import { Link } from 'expo-router'
 
 const Home = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>PEC Event App</Text>
       <Text style={styles.subtitle}>React 19 + React Native 0.79.5</Text>
+
+      <Link href="/login">Login Page</Link>
+
       <TestRefComponent />
     </View>
   )
