8d41dce3cf5851266b487e189c4793d16d29d01b
Haripriya B <priyaramya2930@gmail.com>
2025-08-19 12:30:54 +0530

    feat: added new components and updated create event

---
+++ frontend/constants/Colors.ts
@@ -0,0 +1,22 @@
+const Colors = {
+  light: {
+    text: '#000',
+    background: '#fff',
+    tint: '#2f95dc',
+    tabIconDefault: '#ccc',
+    tabIconSelected: '#2f95dc',
+    navBackground: '#fff',
+    title: '#000',
+  },
+  dark: {
+    text: '#fff',
+    background: '#000',
+    tint: '#fff',
+    tabIconDefault: '#ccc',
+    tabIconSelected: '#fff',
+    navBackground: '#000',
+    title: '#fff',
+  },
+};
+
+export { Colors }; 
